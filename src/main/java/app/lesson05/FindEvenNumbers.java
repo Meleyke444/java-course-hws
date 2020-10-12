e54b31fdea26c50a818090879e22d4f60c25b92b
Meleyke444 <meleyke.qurbanova.95@inbox.ru>
2020-10-12 09:39:20 -0700

    first hw

---
+++ src/main/java/app/lesson05/FindEvenNumbers.java
@@ -0,0 +1,9 @@
+package app.lesson05;
+
+public class FindEvenNumbers {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i%2 == 0) System.out.println(i);
+        }
+    }
+}
