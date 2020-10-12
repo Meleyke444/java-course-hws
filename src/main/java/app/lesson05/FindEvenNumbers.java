6c34a3b3ece5f98d28ccc54dd6eab509db8b4064
Meleyke444 <meleyke.qurbanova.95@inbox.ru>
2020-10-12 09:42:07 -0700

    "initial commit"

--- src/main/java/app/lesson05/FindEvenNumbers.java
+++ src/main/java/app/lesson05/FindEvenNumbers.java
@@ -1,6 +1,7 @@
 package app.lesson05;
 
 public class FindEvenNumbers {
+    // first hw
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             if (i%2 == 0) System.out.println(i);
