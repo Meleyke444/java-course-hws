b7441db078fc5443ad33bf1be3ee150ef77b1fd2
Meleyke444 <meleyke.qurbanova.95@inbox.ru>
2020-11-20 04:05:37 -0800

    Game1App is add

--- src/main/java/app/hw_game_tasks/Game1App.java
+++ src/main/java/app/hw_game_tasks/Game1App.java
@@ -1,2 +1,24 @@
-package app.hw_game_tasks;public class Game1App {
+package app.hw_game_tasks;
+
+import java.util.Scanner;
+
+public class Game1App {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Let the game begin!");
+        System.out.println("====================");
+        int randomNumber=(int) (Math.random()*100);
+         while (scan.hasNext()){
+             int num= scan.nextInt();
+             if (num<randomNumber) System.out.println("Your number is too small. Please, try again.");
+             else if (num>randomNumber) System.out.println("Your number is too big. Please, try again.");
+             else if (num==randomNumber)  {
+                 System.out.println("======================");
+                 System.out.println("Congratulations !!!");
+                 System.out.println("======================");
+                 break;
+             }
+         }
+
+    }
 }
